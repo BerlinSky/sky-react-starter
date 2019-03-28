2a7eed1f1871ea0b749ce2486750f2b00ef0b18d
Berlin Sky <pedroyoung8989@gmail.com>
2019-03-28 16:39:40 -0700

    updates

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react'
 
+import './Home.css'
+
 class Home extends Component {
   render() {
-    return <h1>Hier liegt Home Component</h1>
+    return (
+      <div className="Home">
+        <h1>Welcom Home</h1>
+      
+        <p>
+          C'est la vie <br></br>
+          <a href="http://www.google.com">google</a>
+        </p>
+
+        <p>
+          <button style={{ backgroundColor: 'gray', border: '1px solid black' }}>
+            Click me!
+          </button>
+        </p>
+
+      </div>
+    )
   }
 }
 
