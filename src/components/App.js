d34de94ec974667d2bd0187e9c1c3a26ba21a3ed
Berlin Sky <pedroyoung8989@gmail.com>
2019-04-02 22:03:01 -0700

    updates

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,8 @@ import React, { Component } from 'react'
 // import Todo from './Todo/Todo'
 // import Timer from './Pomodoro/Timer'
 // import Coins from './Coins/Coins'
-import Chart from './Chart/Chart'
+// import Chart from './Chart/Chart'
+import Animation from './Animation/Animation'
 
 import Header from '../shared/components/layout/Header'
 import Content from '../shared/components/layout/Content'
@@ -13,44 +14,13 @@ import Footer from '../shared/components/layout/Footer'
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      chartType: 'line'
-    }
-
-    this.columns = [
-      ['BTC', 3000, 6000, 10000, 15000, 13000, 11000],
-      ['ETH', 2000, 3000, 5000, 4000, 3000, 940],
-      ['XRP', 100, 200, 300, 500, 400, 300],
-    ]
-  }
-
-  setBarChart = () => {
-    this.setState({
-      chartType: 'bar'
-    })
-  }
-
-  setLineChart = () => {
-    this.setState({
-      chartType: 'line'
-    })
-  }
-
+  
   render() {
     return (
       <div className="App">
         <Header title="Welcome to SKY Club" />
         <Content>
-          <Chart columns={ this.columns } chartType={ this.state.chartType }/>
-          
-          <p>
-            Chart Type
-            <button onClick={ this.setBarChart }>Bar</button>
-            <button onClick={ this.setLineChart }>Line</button>
-          </p>
+          <Animation />
         </Content>
         <Footer />
       </div>
