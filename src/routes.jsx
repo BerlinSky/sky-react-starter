8abfaedc637108ee7459ee790962e7748b0144d3
Berlin Sky <pedroyoung8989@gmail.com>
2019-04-09 16:18:44 -0700

    updates

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import App from './components/App'
 import Home from './components/Home'
+import Error404 from './components/Error/404'
 
 const AppRoutes = () => (
   <App>
-    <Route path="/" component={ Home } />
+    <Switch>
+      <Route path="/" component={ Home } exact />
+      <Route component={ Error404 } />
+    </Switch>
   </App>
 )
 
