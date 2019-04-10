49eeef9d3c467bb629961e516ec88f1fd4f8e60e
Berlin Sky <pedroyoung8989@gmail.com>
2019-04-09 17:02:54 -0700

    updates

--- src/components/Notes/index.jsx
+++ src/components/Notes/index.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import './Notes.css'
+import { notDeepEqual } from 'assert';
 
 class Notes extends Component {
   constructor() {
@@ -22,10 +24,36 @@ class Notes extends Component {
       ]
     }
   }
+
+  renderNotes = notes => (
+    <ul>
+      { notes.map(( note, key ) => (
+        <li key={ key }>
+          <Link to={ `/notes/${note.id}` }>{ note.title }</Link>
+        </li>
+      ))}
+    </ul>
+  )
+
   render() { 
+    console.log(this.props);
+
+    const { match: { params: { noteId }} } = this.props
+    const { notes } = this.state
+    
+    let selectedNote = false
+
+    if (noteId > 0) {
+      selectedNote = notes.filter(
+        note => note.id === Number(noteId)
+      )
+    }
+
     return (  
       <div className="Notes">
         <h1>Notes</h1>
+
+        {this.renderNotes(selectedNote || notes)}
       </div>
     )
   }
